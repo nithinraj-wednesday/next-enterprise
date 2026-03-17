cfd03feeba644d42f91daedee7545b3110fb73d5
Nithin Raj <nithin.raj@wednesday.is>
2026-03-17 11:47:48 +0530

    refactor: remove explicit PostHog initialization from provider.

--- components/providers/PostHogProvider.tsx
+++ components/providers/PostHogProvider.tsx
@@ -2,22 +2,7 @@
 
 import posthog from "posthog-js"
 import { PostHogProvider as PostHogProviderReact } from "posthog-js/react"
-import { useEffect } from "react"
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const token = process.env.NEXT_PUBLIC_POSTHOG_TOKEN
-      if (token) {
-        posthog.init(token, {
-          api_host: "/ingest",
-          ui_host: "https://eu.posthog.com",
-          capture_pageview: false,
-          capture_exceptions: true,
-        })
-      }
-    }
-  }, [])
-
   return <PostHogProviderReact client={posthog}>{children}</PostHogProviderReact>
 }
